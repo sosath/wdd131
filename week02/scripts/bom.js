31c03e6c7f382311d96eb3b992912bc37b9cbae5
sosath <sosalijeronthomas@gmail.com>
2025-11-12 13:31:12 -0400

    update

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,30 +1,44 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('#addButton');
-const list = document.querySelector('#list');
+// I added the functionality of clicking when pressing 'Enter' as well.
+document.addEventListener('DOMContentLoaded', () => {
 
-button.addEventListener('click', () => {
-  const chapter = input.value.trim();
-  if (chapter === '') {
-    alert('Please enter a chapter!');
-    return;
+  const input = document.querySelector('#favchap');
+  const button = document.querySelector('#addButton');
+  const list = document.querySelector('#list');
+
+  function addChapter() {
+    const chapter = input.value.trim();
+
+    if (chapter !== '') {
+      const li = document.createElement('li');
+      const deleteButton = document.createElement('button');
+
+      li.textContent = chapter;
+      deleteButton.textContent = '❌';
+      deleteButton.setAttribute('aria-label', `Remove ${chapter}`);
+      deleteButton.classList.add('delete');
+
+      li.append(deleteButton);
+      list.append(li);
+
+      input.value = '';
+      input.focus();
+
+      deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+        input.focus();
+      });
+
+    } else {
+      alert('Please enter a chapter before adding!');
+      input.focus();
+    }
   }
 
-  const li = document.createElement('li');
-  const deleteButton = document.createElement('button');
+  button.addEventListener('click', addChapter);
 
-  li.textContent = chapter;
-  deleteButton.textContent = '❌';
-  deleteButton.setAttribute('aria-label', `Remove ${chapter}`);
-  deleteButton.classList.add('delete');
-
-  li.append(deleteButton);
-
-  list.append(li);
-
-  input.value = '';
-  input.focus();
-
-  deleteButton.addEventListener('click', () => {
-    list.removeChild(li);
+  input.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      addChapter();
+    }
   });
 });
