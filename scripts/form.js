b919c289f7154f0b51ff055a1106b40be6329580
sosath <sosalijeronthomas@gmail.com>
2026-02-10 21:32:56 -0400

    last

--- scripts/form.js
+++ scripts/form.js
@@ -1,9 +1,9 @@
 const products = [
-    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+    { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
